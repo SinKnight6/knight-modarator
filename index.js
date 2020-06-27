d9f271f017b29f4d9e19e0aa02fe17fb3576bb84
SinKnight6 <sinnersquadhq@gmail.com>
2020-06-27 14:26:11 -0500

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 
 const token = process.env.token;
 
+const weather = require('weather-js');
+
 const PREFIX = '$';
 
 const bot = new Discord.Client();
@@ -227,6 +229,33 @@ bot.on('message', async function(message) {
   }
 
 });
+
+// Break 
+
+if (message.content.startsWith('WEATHER') || (message.content.startsWith('weather'))){
+  weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result){
+    if (err) message.channel.send(err);
+    if (result.length === 0){
+      message.channel.send('**Please enter a valid location.**')
+      return;
+    }
+    var current = result[0].current;
+    var location = result[0].location;
+    const uEmbed = new Discord.MessageEmbed()
+      .setDescription(`**${current.skytext}**`)
+      .setAuthor(`Weather for ${current.observationpoint}`)
+      .setThumbnail(current.imageUrl)
+      .setColor(0x00AE86)
+      .addField(`Timezone`,`UTC${location.timezone}`, true)
+      .addField(`Degree Type`,location.degreetype, true)
+      .addField(`Temperature`,`${current.temperature} Degrees`, true)
+      .addField(`Feels Like`, `${current.feelslike} Degree`,true)
+      .addField(`Winds`,current.winddisplay, true)
+      .addField(`Humidity`, `${current.humidity}%`, true)
+      message.channel.send({embed: uEmbed});
+
+  });
+}
 
 // Break 
 
