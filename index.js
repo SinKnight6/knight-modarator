275c558d5a7e20a855a2f5ccedb2ff1a2eced9b3
SinKnight6 <61901906+SinKnight6@users.noreply.github.com>
2022-04-02 12:40:06 -0500

    new update

--- index.js
+++ index.js
@@ -78,31 +78,31 @@ bot.on('message', async function(message) {
     });
      
 
-    }
-    else if(isValidCommand(message, "del")) {
-      message.delete()
-    let args = message.content.toLowerCase().substring(5);
-    let roleNames = args.split(", ");
-    let roleSet = new Set(roleNames);
-    let { cache } = message.guild.roles; 
-    roleSet.forEach(roleName => {
-      let role = cache.find(role => role.name.toLowerCase() === roleName);
-    if(role) {
-      if(message.member.roles.cache.has(role.id)) {
-        message.member.roles.remove(role)
-        .then(member => message.channel.send("You were removed from this role!"))
-        .catch(err => {
-          console.log(err);
-          message.channel.send("Something went wrong....");
-        });
+    // }
+    // else if(isValidCommand(message, "del")) {
+    //   message.delete()
+    // let args = message.content.toLowerCase().substring(5);
+    // let roleNames = args.split(", ");
+    // let roleSet = new Set(roleNames);
+    // let { cache } = message.guild.roles; 
+    // roleSet.forEach(roleName => {
+    //   let role = cache.find(role => role.name.toLowerCase() === roleName);
+    // if(role) {
+    //   if(message.member.roles.cache.has(role.id)) {
+    //     message.member.roles.remove(role)
+    //     .then(member => message.channel.send("You were removed from this role!"))
+    //     .catch(err => {
+    //       console.log(err);
+    //       message.channel.send("Something went wrong....");
+    //     });
       
-      }
-    } 
-    else {
-      message.channel.send("Role not found!");
-    }
+    //   }
+    // } 
+    // else {
+    //   message.channel.send("Role not found!");
+    // }
 
-    });
+    // });
 
   }
   else if (isValidCommand(message, "embed")) {
